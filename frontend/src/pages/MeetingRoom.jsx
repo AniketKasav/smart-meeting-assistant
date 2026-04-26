307ea882d1b835512b957e8df9eaaf94e8d3dc4d
unknown <aniketkasav789@gmail.com>
2026-04-26 23:23:45 +0530

    Fix signaling collision: user-joined back to initiator false

--- frontend/src/pages/MeetingRoom.jsx
+++ frontend/src/pages/MeetingRoom.jsx
@@ -424,7 +424,7 @@ const MeetingRoom = () => {
             });
           });
           globalSocket.on("user-joined", (participant) => {
-            createPeer(participant, true);
+            createPeer(participant, false);
           });
 
           globalSocket.on("offer", ({ from, offer }) => {
@@ -1350,6 +1350,7 @@ const MeetingRoom = () => {
 };
 
 export default MeetingRoom;
+
 
 
 
