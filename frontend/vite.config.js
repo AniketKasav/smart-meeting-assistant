d74d6a0087b53a71dcff2b7dabcfb832c0d9569b
unknown <aniketkasav789@gmail.com>
2026-04-26 23:12:45 +0530

    Add node polyfills for SimplePeer Vite fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer', 'process', 'util', 'stream', 'events'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   define: {
-    // Fix for simple-peer and other Node packages
     global: 'globalThis',
     'process.env': {}
   },
   optimizeDeps: {
     esbuildOptions: {
-      // Node.js global to browser globalThis
       define: {
         global: 'globalThis'
       }
